885611f3c006c992440db35fb8e6019e7c237c64
Quân Nguyễn Anh <anhquan310399@gmail.com>
2021-05-01 18:47:10 +0700

    zoom chat and video

---
+++ src/Layouts/zoom-meeting/components/videoContainer.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import "./videoContainer.css"
+const VideoContainer = ({id, stream }) => {
+
+    const fullScreen = (e) => {
+        const currentElement = e.currentTarget
+
+        if (currentElement) {
+            currentElement.classList.toggle('fullsize-screen-item')
+        }
+    }
+
+    useEffect(() => {
+        if (stream) {
+            const video = document.getElementById(id);
+            video.srcObject = stream
+            video.addEventListener('loadedmetadata', () => {
+                video.play()
+            });
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [stream])
+
+    return (
+        <>
+            <div style={{ width: 200, height: 150, background: 'blue', position: 'relative' }} onClick={(e) => fullScreen(e)}>
+                <div className="full-screen">
+                    <i className="full-screen-widget__icon"></i>
+                </div>
+                <video id={id}></video>
+            </div>
+        </>
+    )
+}
+
+export default VideoContainer;
